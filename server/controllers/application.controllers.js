c89e817b1b1fdc3c3078f63886274b97fab0eaa1
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-06-25 01:47:51 +0500

    create get applicants controller for recuriter

--- server/controllers/application.controllers.js
+++ server/controllers/application.controllers.js
@@ -94,4 +94,31 @@ export const getAppliedJobs = async (req, res) => {
   }
 };
 
+// role === "recruiter"
+export const getApplicants = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+
+    const job = await Job.findById(jobId).populate({
+      path: "applications",
+      options: { sort: { createdAt: -1 } },
+      populate: {
+        path: "applicant",
+      },
+    });
+    if (!job) {
+      return res.status(404).json({
+        message: "Job not found",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      job,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
