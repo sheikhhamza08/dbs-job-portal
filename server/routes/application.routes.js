bfd5f90209c1486b6b08c9be07c7e84cf7b74cad
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-06-25 01:46:18 +0500

    add get applied jobs route

--- server/routes/application.routes.js
+++ server/routes/application.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import {
   applyJob,
+  getAppliedJobs,
 } from "../controllers/application.controllers.js";
 import authUser from "../middlewares/authUser.js";
 
 const applicantionRouter = express.Router();
 
 applicantionRouter.post("/apply/:id", authUser, applyJob);
+applicantionRouter.get("/get", authUser, getAppliedJobs);
+
 export default applicantionRouter;
