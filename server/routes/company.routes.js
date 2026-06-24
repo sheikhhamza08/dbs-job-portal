b6b3d207bc19371e41359ea218187af7382cf352
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-06-25 01:38:11 +0500

    add get company details by user route

--- server/routes/company.routes.js
+++ server/routes/company.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getCompanyDetailsByUser,
   registerCompany,
 } from "../controllers/company.controllers.js";
 import authUser from "../middlewares/authUser.js";
@@ -8,5 +9,6 @@ import { singleUpload } from "../middlewares/multer.js";
 const companyRouter = express.Router();
 
 companyRouter.post("/register", authUser, registerCompany);
+companyRouter.get("/get", authUser, getCompanyDetailsByUser);
 
 export default companyRouter;
