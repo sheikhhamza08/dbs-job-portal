4da57b11ae7bbdeb09fa59367a73ff54e342c7d8
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-06-25 01:40:51 +0500

    create update  company  controller

--- server/controllers/company.controllers.js
+++ server/controllers/company.controllers.js
@@ -83,3 +83,44 @@ export const getCompanyDetailsById = async (req, res) => {
   }
 };
 
+export const updateCompany = async (req, res) => {
+  try {
+    const { companyName, description, website, location } = req.body;
+    const file = req.file;
+
+    // cloudinary
+    let logo = null;
+
+    if (file) {
+      const fileUri = getDataUri(file);
+      const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+      logo = cloudResponse.secure_url;
+    }
+
+    const updateData = {
+      companyName,
+      description,
+      website,
+      location,
+      logo,
+    };
+
+    const company = await Company.findByIdAndUpdate(req.params.id, updateData, {
+      new: true,
+    });
+    if (!company) {
+      return res.status(404).json({
+        message: "Company not found",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Company information updated",
+      company,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
