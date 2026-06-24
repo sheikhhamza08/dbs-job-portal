04d1ea86c6f89eae0e7317202e6a5d39bfe545a6
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-06-25 01:49:39 +0500

    add update status router

--- server/routes/application.routes.js
+++ server/routes/application.routes.js
@@ -3,6 +3,7 @@ import {
   applyJob,
   getApplicants,
   getAppliedJobs,
+  updateStatus,
 } from "../controllers/application.controllers.js";
 import authUser from "../middlewares/authUser.js";
 
@@ -11,5 +12,6 @@ const applicantionRouter = express.Router();
 applicantionRouter.post("/apply/:id", authUser, applyJob);
 applicantionRouter.get("/get", authUser, getAppliedJobs);
 applicantionRouter.get("/:id/applicants", authUser, getApplicants);
+applicantionRouter.post("/status/:id/update", authUser, updateStatus);
 
 export default applicantionRouter;
