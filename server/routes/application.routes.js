f399b01d1eca0c65ee014353cc833a918ba3cf45
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-06-25 01:48:38 +0500

    add get applicants route

--- server/routes/application.routes.js
+++ server/routes/application.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   applyJob,
+  getApplicants,
   getAppliedJobs,
 } from "../controllers/application.controllers.js";
 import authUser from "../middlewares/authUser.js";
@@ -9,5 +10,6 @@ const applicantionRouter = express.Router();
 
 applicantionRouter.post("/apply/:id", authUser, applyJob);
 applicantionRouter.get("/get", authUser, getAppliedJobs);
+applicantionRouter.get("/:id/applicants", authUser, getApplicants);
 
 export default applicantionRouter;
